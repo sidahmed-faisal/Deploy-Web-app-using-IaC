a706226b1fff37421c826c04f887e7b95eb1fe89
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2024-01-16 16:57:42 +0400

    Adding Nate Gateways and Route Tables

--- Scripts/create.sh
+++ Scripts/create.sh
@@ -1,5 +1,5 @@
-aws cloudformation create-stack --stack-name %1 \ 
---template-body file://%2  \
---parameters file://%3 \
---capabilities "CAPABILITY_NAMED_IAM" \
---region=us-east-1
+aws cloudformation create-stack --stack-name $1 \
+    --template-body file://App/$2   \
+    --parameters file://App/$3  \
+    --capabilities "CAPABILITY_NAMED_IAM"  \
+    --region=us-east-1
