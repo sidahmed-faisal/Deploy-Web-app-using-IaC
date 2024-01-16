647aed6764e58d7ae9f02521253e2c5edd012403
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2024-01-16 17:05:13 +0400

    added outputs to the template file

--- Scripts/update.sh
+++ Scripts/update.sh
@@ -1,5 +1,5 @@
 aws cloudformation update-stack --stack-name $1  \
-    --template-body file://$2   \
-    --parameters file://$3  \
+    --template-body file://App/$2   \
+    --parameters file://App/$3  \
     --capabilities "CAPABILITY_NAMED_IAM"  \
     --region=us-east-1
