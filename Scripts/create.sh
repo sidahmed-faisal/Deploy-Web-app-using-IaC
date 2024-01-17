e0f4a8f4f9f19482cd7997ea33cf124352cfebd1
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2024-01-17 17:56:34 +0400

    modified create script

--- Scripts/create.sh
+++ Scripts/create.sh
@@ -3,3 +3,6 @@ aws cloudformation create-stack --stack-name $1 \
     --parameters file://App/$3  \
     --capabilities "CAPABILITY_NAMED_IAM"  \
     --region=us-east-1
+
+# ./Scripts/create.sh infrastack /infrastructure_template.yml /parameters.json
+# ./Scripts/create.sh applicationstack /Application_template.yml /application_params.json
